ecd574a19e8f3a1c8d164c5e567631933751f711
Geir Sonnesyn <geirsonnesyn@gmail.com>
2023-09-01 13:03:50 +0200

    converted fetch from using .then to async await

--- js/fetch.js
+++ js/fetch.js
@@ -1,50 +1,48 @@
-// fetch API into a variable
-const apiUrl = "https://api.noroff.dev/api/v1/gamehub";
-// Response from API with simple error handling
-fetch(apiUrl)
-  .then((resp) => {
-    //error checking
-    //200-299 range
-    if (!resp.ok) throw new Error("4-oh-4, Oops something went wrong here");
-    return resp.json();
-  })
-  .then((dataArray) => {
-    //console.log(dataArray);
-    // Get the parent div where card is to be placed.
+async function fetchData() {
+  try {
+    const apiUrl = "https://api.noroff.dev/api/v1/gamehub";
+    const resp = await fetch(apiUrl);
+
+    if (!resp.ok) {
+      throw new Error("4-oh-4, Oops something went wrong here");
+    }
+
+    const dataArray = await resp.json();
+
     let buy = document.getElementById("dataArray");
-    // Inject the HTML
     buy.innerHTML = dataArray
       .map(({ id, title, description, image, price }) => {
         return `<div class="card-container">
-      <div class="card" data-id="${id}">
-        <h4>${title}</h4>
-        <img class="card-img" src="${image}" alt="${description}">
-        <div class="modal-container">
-          <input class="modal-toggle" id="${id}" type="checkbox">
-          <button class="button">Read More</button>
-          <div class="modal-backdrop">
-            <div class="modal-content">
-              <label class="modal-close" for="${id}">X</label>
-              <h2>${title}</h2>
-              <hr />
-              <p>${description}</p>
-              <hr>
-              <p class="price">$ ${price}</p>
-              <hr>
-              <img class="card-img" src="${image}" alt="${description}">
-              <hr>
-              <button class="card-button"><a class="card-link" href="../html/cart.html">Add To Cart</a></button>
+          <div class="card" data-id="${id}">
+            <h4>${title}</h4>
+            <img class="card-img" src="${image}" alt="${description}">
+            <div class="modal-container">
+              <input class="modal-toggle" id="${id}" type="checkbox">
+              <button class="button">Read More</button>
+              <div class="modal-backdrop">
+                <div class="modal-content">
+                  <label class="modal-close" for="${id}">X</label>
+                  <h2>${title}</h2>
+                  <hr />
+                  <p>${description}</p>
+                  <hr>
+                  <p class="price">$ ${price}</p>
+                  <hr>
+                  <img class="card-img" src="${image}" alt="${description}">
+                  <hr>
+                  <button class="card-button"><a class="card-link" href="../html/cart.html">Add To Cart</a></button>
+                </div>
+              </div>
             </div>
+            <button class="card-button"><a class="card-link" href="../html/cart.html">Add To Cart</a></button>
           </div>
-        </div>
-        <button class="card-button"><a class="card-link" href="../html/cart.html">Add To Cart</a></button>
-      </div>
-    </div>`;
+        </div>`;
       })
       .join("");
-  })
-  .catch((err) => {
+  } catch (err) {
     let error = document.getElementById("browse");
     error.innerText = err;
-    //console.warn(err.message);
-  });
+  }
+}
+
+fetchData();
